0ad158e7c131993de36bad280c6c30e7162b6f08
michellewidjaja <michelle.widjaja@gmail.com>
2025-05-05 12:10:05 +0700

    Fix favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
 import "./globals.scss";
+import Head from "next/head"; 
 
 export const metadata: Metadata = {
   title: "Michelle Lee Widjaja Portfolio",
@@ -14,12 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
         <link rel="icon" href="/my-favicon.ico" />
-      </head>
+      </Head>
       <body>
         {children}
       </body>
